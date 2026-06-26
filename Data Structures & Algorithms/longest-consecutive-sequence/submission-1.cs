15d966bd405bffdf4c344b1c76288541730892b9
Sharad Tanwar <sharad_tanwar@redmane.com>
2026-06-25 22:45:48 -0500

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public int LongestConsecutive(int[] nums) {
+        var result = 0;
+
+        var lookupSet = new HashSet<int>();
+        for (var i =0; i < nums.Length; i++){
+            lookupSet.Add(nums[i]);
+        }
+
+        for (var i=0; i< nums.Length; i++){
+            var current = nums[i];
+            if (!lookupSet.Contains(current - 1)){
+                var total = 1;
+                while(lookupSet.Contains(current + 1)){
+                    total = total + 1;
+                    current = current + 1;
+                }
+
+                if (total > result){
+                    result= total;
+                }
+            }
+            
+        }
+
+        return result;
+    }
+}
